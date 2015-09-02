3b6333a509a84bd9c1c771fa806c5761945c05c9
Ben Kero <ben.kero@gmail.com>
2015-09-02 16:26:46 -0700

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,24 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2048
+TARGET_SCREEN_WIDTH := 1536
+TARGET_BOOTANIMATION_HALF_RES := false
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/teclast/x98pro/full_x98pro.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := x98pro
+PRODUCT_NAME := cm_x98pro
+PRODUCT_BRAND := teclast
+PRODUCT_MODEL := X98_Pro
+PRODUCT_MANUFACTURER := teclast
+
+PRODUCT_RUNTIMES := runtime_libart_default
+
+#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.0.1/LRX22C/1602158:user/release-keys PRIVATE_BUILD_DESC="hammerhead-user 5.0.1 LRX22C 1602158 release-keys"
+
+# Enable Torch
+#PRODUCT_PACKAGES += Torch
---
+++ device_x98pro.mk
@@ -0,0 +1,55 @@
+#
+# Copyright 2013 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_DEVICE := x98pro
+PRODUCT_NAME := x98pro
+
+PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# DENSITY IS ACTUALLY 264 (2048*1536, 9.7inch)
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    ro.sf.lcd_density=480
+
+## call dalvik heap config
+#$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+
+## call hwui memory config
+#$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+
+#DEVICE_PACKAGE_OVERLAYS := \
+#    device/asus/Z00A/overlay
+
+#PRODUCT_COPY_FILES += \
+#    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+#    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
+
+# NFC
+#PRODUCT_COPY_FILES += \
+#    device/asus/Z00A/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+#    device/asus/Z00A/nfc/libnfc-brcm-20795a20.conf:system/etc/libnfc-brcm-20795a20.conf
+
+# NFC packages
+#PRODUCT_PACKAGES += \
+#    nfc_nci.bcm2079x.default \
+#    NfcNci \
+#    Tag
+
+#$(call inherit-product-if-exists, vendor/asus/Z00A/Z00A-vendor.mk)
+
+# Inherit from mofd-common
+#$(call inherit-product, device/asus/mofd-common/mofd.mk)
+
